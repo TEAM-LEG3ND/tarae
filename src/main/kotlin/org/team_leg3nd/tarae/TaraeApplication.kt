8a65ae44c1ae522066218ea062158f713aebf5ba
Beopsik <mbsik6082@naver.com>
2024-07-24 21:58:52 +0900

    Init

---
+++ src/main/kotlin/org/team_leg3nd/tarae/TaraeApplication.kt
@@ -0,0 +1,11 @@
+package org.team_leg3nd.tarae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaraeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaraeApplication>(*args)
+}
---
+++ src/test/kotlin/org/team_leg3nd/tarae/TaraeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.team_leg3nd.tarae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaraeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
