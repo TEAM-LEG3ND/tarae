b130c2aecb25ba335b561499d5e763b5cf9fe7d6
redjen8 <seokwoo2525@gmail.com>
2024-09-08 23:45:38 +0900

    Feat: jpa 설정 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.hibernate.validator:hibernate-validator:6.2.0.Final")
+    implementation("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
