6050404ba9904d52361e067e1bbfcbfdcd077ed4
Beopsik <mbsik6082@naver.com>
2024-09-22 17:49:49 +0900

    feat: add security config

---
+++ src/main/kotlin/org/team_leg3nd/tarae/config/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package org.team_leg3nd.tarae.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { authz ->
+                authz
+                    .requestMatchers("/api/login").permitAll()
+                    .anyRequest().authenticated()
+            }
+
+        return http.build()
+    }
+}
+
