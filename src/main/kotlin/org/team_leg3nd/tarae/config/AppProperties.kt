ce877ca8d59ed66a2d280ea386ef73687d66a95c
Beopsik <mbsik6082@naver.com>
2024-09-22 17:48:41 +0900

    feat: add local config

---
+++ src/main/kotlin/org/team_leg3nd/tarae/config/AppProperties.kt
@@ -0,0 +1,19 @@
+package org.team_leg3nd.tarae.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "")
+data class AppProperties(
+    val server:Server = Server(),
+    val login: Login = Login()
+) {
+    data class Server(
+        var port: Int = 8080
+    )
+    data class Login(
+        var oauthServerUrl: String = "",
+        var redirectUri: String = ""
+    )
+}
